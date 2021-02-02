eced25568f68c0d3774f41128fae016ceb490ff7
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-02 18:08:03 +0000

    implemented ISecurity in rollercoaster and passed two related tests

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,6 +2,7 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,5 +28,17 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void visitorAllowed() {
+        Visitor visitor = new Visitor(60, 156, 10000);
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorNotAllowed() {
+        Visitor visitor = new Visitor(6, 101, 1);
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor));
     }
 }
